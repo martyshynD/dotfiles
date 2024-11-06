c50000ac40d8e6132594df223bcd1c397a30f7f1
martyshynD <132333148+martyshynD@users.noreply.github.com>
2024-11-06 22:15:21 +0200

    Add files via upload

---
+++ fehbg.sh
@@ -0,0 +1,3 @@
+# feh --bg-scale /home/fortis/Downloads/joshua-sortino-f3uWi9G-lus-unsplash.jpg
+# feh --bg-scale /home/fortis/Pictures/Wallpapers/ironman.jpg
+feh --bg-scale /home/fortis/Pictures/Wallpapers/neon_nadpis_stena_134368_1366x768.jpg
